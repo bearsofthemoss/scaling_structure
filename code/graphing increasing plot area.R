80153182b14cb31c33ccb376f4114391117381c6
Alex Young <bearsofthemoss@gmail.com>
2020-12-16 09:26:32 -0500

    graphing, changing 40m to 40 m

--- code/graphing increasing plot area.R
+++ code/graphing increasing plot area.R
@@ -3,15 +3,34 @@ library(ggplot2)
 
 
 # pull in different plot areas and graph them.
-m40<-read.csv("output_data/40m_plot_area.csv")
-m100<-read.csv("output_data/100m_plot_area.csv")
-m250<-read.csv("output_data/250m_plot_area.csv")
-m500<-read.csv("output_data/500m_plot_area.csv")
+m40<-read.csv("output_data/OSBS/40m_plot_area.csv")
+m100<-read.csv("output_data/OSBS/100m_plot_area.csv")
+m250<-read.csv("output_data/OSBS/250m_plot_area.csv")
+m500<-read.csv("output_data/OSBS/500m_plot_area.csv")
+#
+OSBS<-rbind(m40, m100, m250, m500)
+OSBS$site<-"OSBS"
+###############################
+m40<-read.csv("output_data/UNDE/40m_plot_area.csv")
+m100<-read.csv("output_data/UNDE/100m_plot_area.csv")
+m250<-read.csv("output_data/UNDE/250m_plot_area.csv")
+m500<-read.csv("output_data/UNDE/500m_plot_area.csv")
+#
+UNDE<-rbind(m40, m100, m250)
+UNDE$site<-"UNDE"
+###############################
+m10<-read.csv("output_data/WREF/40m_plot_area.csv")
+m40<-read.csv("output_data/WREF/40m_plot_area.csv")
+m100<-read.csv("output_data/WREF/100m_plot_area.csv")
+m250<-read.csv("output_data/WREF/250m_plot_area.csv")
+m500<-read.csv("output_data/WREF/500m_plot_area.csv")
+#
+WREF<-rbind(m40, m100, m250, m500)
+WREF$site<-"WREF"
 
+fsd<-rbind(WREF, OSBS, UNDE)
 
-
-fsd<-rbind(m40,m100, m250,m500)
-fsd$plot_area<-factor(fsd$plot_area, levels=c("40m","100 m", "250 m","500 m"))
+fsd$plot_area<-factor(fsd$plot_area, levels=c("40 m","100 m", "250 m","500 m"))
 table(fsd$metric)
 
 head(fsd)
@@ -19,14 +38,16 @@ library(tidyr)
 spr<-spread(fsd, "metric","value")
 names(spr)
 
-sp<-spr[ ,c(2,3,4,6,7,10,11,12,14)]
+sp<-spr[ ,c(2,3,4,5,6,7,10,11,12,14)]
 
 head(sp)
 
-use<-gather(sp, "metric","value",4:9)
+use<-gather(sp, "metric","value",5:10)
 
-
-ggplot(use, aes(x=plot_area, y=value+0.1))+geom_boxplot()+facet_wrap(~metric, scales="free_y")+scale_y_log10()+
+library(ggplot2)
+ggplot(use, aes(x=plot_area, y=value+0.1, fill=site))+geom_boxplot()+facet_wrap(~metric, scales="free_y")+scale_y_log10()+
   theme_bw()+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+theme(text=element_text(size=25))
 
 
+
+write.csv(use, file="output_data/combined_scaling_data.csv")
